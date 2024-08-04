18eb22db6d3d9d7c80e12769aeaf6feeb3be9fe3
Chi Cuong <nguyenchicuongk21@gmail.com>
2024-08-04 10:59:59 +0700

    Add project files.

---
+++ src/Modules/EventModule/EventManagement.Events/Entities/EventModel.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace EventManagement.Events.Entities
+{
+    public class EventModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public DateTime StartsAt { get; set; }
+        public DateTime? EndsAt { get; set; }
+        public EventStatus Status { get; set; }
+        public double TicketPrice { get; set; }
+    }
+
+    public enum EventStatus
+    {
+        Draft = 0,
+        Published = 1,
+        Completed = 2,
+        Cancelled = 3
+    }
+}
